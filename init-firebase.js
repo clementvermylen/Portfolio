19efe6a85ebc18deb2fe251091b5ec3ab9c2ee79
clementv <vermylenclement@gmail.com>
2022-06-21 14:47:53 +0200

    a

--- init-firebase.js
+++ init-firebase.js
@@ -1,10 +1,17 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBgK4ZLhlKD4vvqqAmxHPXPOkW02_3MBDw",
-    authDomain: "bitrupt-trainee-web.firebaseapp.com",
-    databaseURL: "https://bitrupt-trainee-web-default-rtdb.firebaseio.com",
-    projectId: "bitrupt-trainee-web",
-    storageBucket: "bitrupt-trainee-web.appspot.com",
-    messagingSenderId: "151546755005",
-    appId: "1:151546755005:web:f459ab08f7fd0f13b6c4d4",
+    apiKey: "AIzaSyDY5iQxXZG_x3Xpo6A8NQB9xY_MZwDplwk",
+
+    authDomain: "clementvermylen-3a21f.firebaseapp.com",
+  
+    projectId: "clementvermylen-3a21f",
+  
+    storageBucket: "clementvermylen-3a21f.appspot.com",
+  
+    messagingSenderId: "666325267359",
+  
+    appId: "1:666325267359:web:fbb4936bcad3beee2303aa",
+  
+    measurementId: "G-2YCK0T6Y9M"
+  
 };
 const firebase = firebase.initalizeApp(firebaseConfig);
