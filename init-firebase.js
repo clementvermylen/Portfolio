ac178c58ff08402aaf0f6267b6fd3147c979dc1c
clementv <vermylenclement@gmail.com>
2022-06-21 14:30:54 +0200

    cleaned UI, colors, text, added firebase setup

---
+++ init-firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgK4ZLhlKD4vvqqAmxHPXPOkW02_3MBDw",
+    authDomain: "bitrupt-trainee-web.firebaseapp.com",
+    databaseURL: "https://bitrupt-trainee-web-default-rtdb.firebaseio.com",
+    projectId: "bitrupt-trainee-web",
+    storageBucket: "bitrupt-trainee-web.appspot.com",
+    messagingSenderId: "151546755005",
+    appId: "1:151546755005:web:f459ab08f7fd0f13b6c4d4",
+};
+const firebase = firebase.initalizeApp(firebaseConfig);
